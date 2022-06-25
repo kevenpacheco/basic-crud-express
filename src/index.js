12a9c282f12bf2742e797e7b23c7763228550d19
Keven Pacheco <kevenpacheco2000@gmail.com>
2022-06-25 12:16:53 -0300

    feat: create simulation of routes of type GET, POST, PUT, DELETE

--- src/index.js
+++ src/index.js
@@ -2,9 +2,35 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  response.json({ message: 'Hello World' })
-})
+app.get('/projects', (request, response) => {
+  response.json([
+    'Project 1',
+    'Project 2',
+  ])
+});
+
+app.post('/projects', (request, response) => {
+  response.json([
+    'Project 1',
+    'Project 2',
+    'Project 3',
+  ])
+});
+
+app.put('/projects/:id', (request, response) => {
+  response.json([
+    'Project 4',
+    'Project 2',
+    'Project 3',
+  ])
+});
+
+app.delete('/projects/:id', (request, response) => {
+  response.json([
+    'Project 2',
+    'Project 3',
+  ])
+});
 
 app.listen(3333, () => {
   console.log('🚀 Back-end started!')
